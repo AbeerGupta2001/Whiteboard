84d1ef9c3bb7b240e8fb0c01c3269fac3b383a29
AbeerGupta2001 <abeergupta2001@gmail.com>
2025-02-22 13:08:34 +0530

    Third Commit

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -3,15 +3,14 @@ import Canvas from "./_components/canvas";
 import Loading from "./_components/loading";
 
 interface BoardIdPageProps {
-  params: {
-    boardId: string;
-  };
+  params: Promise<{boardId:string}>
 }
 
-const BoardIdPage = ({ params }: BoardIdPageProps) => {
+const BoardIdPage = async({ params }: BoardIdPageProps) => {
+  const { boardId } = await params
   return (
-    <Room roomId={params.boardId} fallback={<Loading/>}>
-      <Canvas boardId={params.boardId} />
+    <Room roomId={boardId} fallback={<Loading/>}>
+      <Canvas boardId={boardId} />
     </Room>
   );
 };
